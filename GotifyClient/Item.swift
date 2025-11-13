dcbd31cf27fea197e0413acb93a7e1a9616ae960
AkagiYui <akagiyui@yeah.net>
2025-11-13 22:51:38 +0800

    Initial Commit

---
+++ GotifyClient/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GotifyClient
+//
+//  Created by kenko on 2025/11/13.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
